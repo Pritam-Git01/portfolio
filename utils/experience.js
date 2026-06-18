fd6c593c086759e3da88d3e76e0e8b356441f590
Pritam Yadav <126752405+Pritam-Git01@users.noreply.github.com>
2026-06-18 22:08:20 +0530

    Rename experienceText function to getExperience

--- utils/experience.js
+++ utils/experience.js
@@ -1,4 +1,4 @@
-export const experienceText = () => {
+export const getExperience = () => {
     const [y, m] = "2.10".split(".").map(Number);
     const nextYear = y + Math.floor((m + 1) / 12);
     const nextMonth = (m + 1) % 12;
